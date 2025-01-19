cdb7ab9ccfb9bf53a3d71f7ce25639730cb5a768
bear320 <aaabear320@gmail.com>
2025-01-19 13:42:21 +0800

    feat: 🎸 Add routes

--- app.ts
+++ app.ts
@@ -1,14 +1,17 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { expensesRoute } from "./routes/expenses";
 
 const app = new Hono();
 
-app.use(logger());
+app.use("*", logger());
 
 app.get("test", (c) => {
   return c.json({ message: "test" });
 });
 
 app.get("/", (c) => c.text("Hono!"));
+
+app.route("/api/expenses", expensesRoute);
 
 export default app;
